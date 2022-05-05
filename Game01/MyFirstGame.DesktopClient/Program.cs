b2f8063164d9f1d21ce55316f7d844a1343af0b1
MattM <67703490+Matthew-Muir@users.noreply.github.com>
2022-05-05 10:01:19 -0700

    Creating new branch for MonoGame Mastery book first tutorial.

--- Game01/MyFirstGame.DesktopClient/Program.cs
+++ Game01/MyFirstGame.DesktopClient/Program.cs
@@ -7,7 +7,7 @@ namespace MyFirstGame.DesktopClient
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new MainGame())
                 game.Run();
         }
     }

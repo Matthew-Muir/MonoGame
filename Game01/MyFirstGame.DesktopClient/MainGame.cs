ca172a6d44196903ab97da35c8209b2822a483c4
MattM <67703490+Matthew-Muir@users.noreply.github.com>
2022-05-06 13:42:00 -0700

    base changes

--- Game01/MyFirstGame.DesktopClient/MainGame.cs
+++ Game01/MyFirstGame.DesktopClient/MainGame.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MyFirstGame.DesktopClient.States;
 
 namespace MyFirstGame.DesktopClient
 {
     public class MainGame : Game
     {
+        private BaseGameState _currentGameState;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private RenderTarget2D _renderTarget;
+        private Rectangle _renderScaleRectangle;
+
+        private const int DESIGNED_RESOLUTION_WIDTH = 640;
+        private const int DESIGNED_RESOLUTION_HEIGHT = 480;
+        private const float DESIGNED_RESOLUTION_ASPECT_RATIO = DESIGNED_RESOLUTION_WIDTH / (float) DESIGNED_RESOLUTION_HEIGHT;
 
         public MainGame()
         {
@@ -18,9 +27,43 @@ namespace MyFirstGame.DesktopClient
 
         protected override void Initialize()
         {
+            _graphics.PreferredBackBufferWidth = 1024;
+            _graphics.PreferredBackBufferHeight = 768;
+            _graphics.IsFullScreen = false;
+            _graphics.ApplyChanges();
+
+            _renderTarget = new RenderTarget2D(_graphics.GraphicsDevice, DESIGNED_RESOLUTION_WIDTH, DESIGNED_RESOLUTION_HEIGHT, false,
+                SurfaceFormat.Color, DepthFormat.None, 0, RenderTargetUsage.DiscardContents);
+
+            _renderScaleRectangle = GetScaleRectangle();
             // TODO: Add your initialization logic here
 
             base.Initialize();
+        }
+
+        private Rectangle GetScaleRectangle()
+        {
+            var variance = 0.5;
+            var actualAspectRatio = Window.ClientBounds.Width / (float) Window.ClientBounds.Height;
+
+            Rectangle scaleRectangle;
+
+            if(actualAspectRatio <= DESIGNED_RESOLUTION_ASPECT_RATIO)
+            {
+                var presentHeight = (int)(Window.ClientBounds.Width / DESIGNED_RESOLUTION_ASPECT_RATIO + variance);
+                var barHeight = (Window.ClientBounds.Height - presentHeight) / 2;
+
+                scaleRectangle = new Rectangle(0, barHeight, Window.ClientBounds.Width, presentHeight);
+            }
+            else
+            {
+                var presentWidth = (int)(Window.ClientBounds.Height * DESIGNED_RESOLUTION_ASPECT_RATIO + variance);
+                var barWidth = (Window.ClientBounds.Width - presentWidth) / 2;
+
+                scaleRectangle = new Rectangle(barWidth, 0 , presentWidth, Window.ClientBounds.Height);
+            }
+
+            return scaleRectangle;
         }
 
         protected override void LoadContent()
@@ -42,7 +85,29 @@ namespace MyFirstGame.DesktopClient
 
         protected override void Draw(GameTime gameTime)
         {
+            //Render to the Render Target
+
+            GraphicsDevice.SetRenderTarget(_renderTarget);
+
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            _currentGameState.Render(_spriteBatch);
+
+            _spriteBatch.End();
+
+            //Now Render the scaled content
+
+            _graphics.GraphicsDevice.SetRenderTarget(null);
+
+            _graphics.GraphicsDevice.Clear(ClearOptions.Target, Color.Black, 1.0f, 0);
+
+            _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Opaque);
+
+            _spriteBatch.Draw(_renderTarget, _renderScaleRectangle, Color.White);
+
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
---
+++ Game01/MyFirstGame.DesktopClient/Objects/BaseGameObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MyFirstGame.DesktopClient.Enum;
+
+namespace MyFirstGame.DesktopClient.Objects
+{
+    public class BaseGameObject
+    {
+        protected Texture2D _texture;
+
+        private Vector2 _position;
+
+        public int zIndex;
+
+        public virtual void OnNotify(Events eventType)
+        {
+
+        }
+
+        public void Render(SpriteBatch spriteBatch)
+        {
+            //TODO: Drawing call here
+            spriteBatch.Draw(_texture, Vector2.One, Color.White);
+        }
+
+    }
+}
